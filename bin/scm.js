b3d18c96794cc0e76cd7a864873b576dcf496f2b
Adam Powers <apowers@ato.ms>
2018-02-25 23:35:52 -0800

    initial command line

---
+++ bin/scm.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var path = require("path");
+
+var {
+    ComponentDirector
+} = require("../index");
+
+var cd = new ComponentDirector();
+
+var globalConfig = "/etc/scm/config.json";
+var userConfig = path.join(process.env.HOME, ".scm/config.json");
+var localConfig = path.join(path.resolve("."),"scm-config.json");
+
+console.log ("Loading", globalConfig, "...");
+cd.loadConfig(globalConfig);
+console.log("Loading", userConfig, "...");
+cd.loadConfig(userConfig);
+console.log ("Loading", localConfig, "...");
+cd.loadConfig(localConfig);
+cd.start()
+    .then(() => {
+        console.log("done?");
+    });
