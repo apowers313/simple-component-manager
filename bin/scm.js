74ceea4640702ddb0e4694e8d0591d184b2449f3
Adam Powers <apowers@ato.ms>
2018-03-24 00:02:49 -0700

    delinting

--- bin/scm.js
+++ bin/scm.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+"use strict";
+
 var path = require("path");
 
 var {
@@ -9,16 +11,16 @@ var {
 var cd = new ComponentDirector();
 
 var globalConfig = "/etc/scm/config.json";
-var userConfig = path.join(process.env.HOME, ".scm/config.json");
-var localConfig = path.join(path.resolve("."),"scm-config.json");
+var userConfig = path.join(process.env.HOME, ".scm/config.json"); // eslint-disable-line no-process-env
+var localConfig = path.join(path.resolve("."), "scm-config.json");
 
-console.log ("Loading", globalConfig, "...");
+console.log("Loading", globalConfig, "...");
 cd.loadConfig(globalConfig);
 console.log("Loading", userConfig, "...");
 cd.loadConfig(userConfig);
-console.log ("Loading", localConfig, "...");
+console.log("Loading", localConfig, "...");
 cd.loadConfig(localConfig);
 cd.start()
     .then(() => {
-        console.log("done?");
+        console.log("Running...");
     });
