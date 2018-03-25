445f02ccdd01e6dd9dd5ced48fd1d0d133281e9d
Adam Powers <apowers@ato.ms>
2018-03-24 17:18:26 -0700

    delint

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+/* eslint-disable global-require */
+
+"use strict";
+
 module.exports = {
-    Component: require ("component-class"),
-    ComponentManager: require ("./lib/component-manager"),
-    ComponentDirector: require ("./lib/component-director"),
-    DefaultLogger: require ("./lib/default-logger")
+    Component: require("component-class"),
+    ComponentManager: require("./lib/component-manager"),
+    ComponentDirector: require("./lib/component-director")
 };
